eced055a36712ee3c3098530a52e66d5cad66aca
Ben Reid <reidben24@gmail.com>
2021-10-30 13:36:47 -0400

    Implement email endpoint. Remove __pycache__ and add to ignore

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,28 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.http.response import HttpResponse, JsonResponse
+from django.http.request import HttpRequest
 
-def test(request):
-    return HttpResponse(f'This is a test: {request.GET["t"]}')
+from .helpers import send_email
+
+SUCCESS_RESPONSE = {
+    'status': 'success'
+}
+
+ERROR_RESPONSE = {
+    'status': 'error'
+}
+
+
+def contact_form(request):
+    # type: (HttpRequest) -> JsonResponse
+    data = request.POST
+    form_fields = ['name', 'reply_email', 'event_type', 'event_date', 'event_time', 'message']
+    for field in form_fields:
+        if field not in data:
+            return JsonResponse(ERROR_RESPONSE)
+    
+    args = {
+        field: data[field]
+        for field in form_fields
+    }
+    result = send_email(**args)
+    return JsonResponse(SUCCESS_RESPONSE) if result else JsonResponse(ERROR_RESPONSE)
